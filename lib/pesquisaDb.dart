c83fda3b93bf4e81df4df04465333173752edf9f
superChokkk <vcestaemchok@gmail.com>
2024-11-13 09:45:08 -0300

    login, falta senha

--- lib/pesquisaDb.dart
+++ lib/pesquisaDb.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<bool> checkEmailOrCpf(String value) async {
